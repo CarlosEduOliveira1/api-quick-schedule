08941c9813bbdcce885096395630272f991ed53b
Carlos Oliveira <carlosoliveira.cee@gmail.com>
2026-03-20 17:49:34 -0300

    Creating the user_id view

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ Projeto back-end para otimizar o agendamento entre clientes e prestadores de ser
 
     pip install -r requirements.txt
 
+    python manage.py migrate *Antes de rodar, é necessário criar a DB.
+
     python manage.py runserver
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view(), name='user_list'),
+    path('', views.UserList.as_view(), name='user_list'),
+    path('<int:pk>/', views.UserDetail.as_view(), name='user_Detail'),
 ]
